f39300d1e7fb40634663de7a9256b68e0e8468d2
Adriana <adrianabeluci@hotmail.com>
2022-03-01 17:50:43 -0300

    alteração

--- equacaoLinear.java
+++ equacaoLinear.java
@@ -1,5 +1,48 @@
+/*  Exercício propostos:
+Escreva um sistema que lê os coeficientes a,b,c,d,e e f 
+e calcula e mostra os valores de x e y. Sabendo que:
+
+ax + by = c 
+dx + ey = f 
+
+Podendo ser resolvido pela seguinte equação: 
+
+x= ce - bf / ae - bd  
+Y= af - cd/ ae - bd  */
+
 package br.com.generation.sequencial;
+
+import java.util.Scanner;
 
 public class equacaoLinear {
 
+		public static void main(String[] args) {
+			
+			Scanner leia= new Scanner(System.in);
+			
+			int a,b,c,d,e,f; 
+			
+			System.out.println(" Digite o 1º número: ");
+			a= leia.nextInt();
+			System.out.println(" Digite o 2º número: ");
+			b= leia.nextInt();
+			System.out.println(" Digite o 3º número: ");
+			c= leia.nextInt();
+			System.out.println(" Digite o 4º número: ");
+			d= leia.nextInt();
+			System.out.println(" Digite o 5º número: ");
+			e= leia.nextInt();
+			System.out.println(" Digite o 6º número: ");
+			f= leia.nextInt();
+
+			double X= (c*e) - (b*f)/ (a*e) - (b*d);
+			double Y= (a*f) - (c*a)/ (a*e) - (b*d);
+
+			System.out.println("\n O valor de X é: " + X);
+			System.out.println("\n O valor de Y é: " + Y);
+			
+			leia.close();
+			
+	}
 }
+
--- equacaoRaiz.java
+++ equacaoRaiz.java
@@ -1,3 +1,10 @@
+/* . Exercício Proposto:
+
+Construa um programa em c que, tendo como dados de entrada dois 
+pontos quaisquer no plano, P(x1, y1) e P(x2, y2), escreva a distância 
+entre eles. A fórmula que efetua tal cálculo é: 
+D= raiz de (x2 - x1)² + (y2 - y1)²;  */
+
 package br.com.generation.sequencial;
 
 import java.util.Scanner;
